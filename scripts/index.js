38253c095e9fbb7bdf21d371ae69fe0298aa2e8d
Anokhina <asya.anokhina@gmail.com>
2022-05-25 20:18:40 +0300

    feat: popup finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,51 @@
- const editButton = document.querySelector('.profile__button-edit');
- const popup = document.querySelector('.popup');
- const closePopButton = document.querySelector('.popup__close');
+const editButton = document.querySelector('.profile__button-edit');
+const popup = document.querySelector('.popup');
+const closePopButton = document.querySelector('.popup__close');
 
- const titleElement = document.querySelector('.profile__name');
- const nameFieldElement = document.querySelector('[name="name"]')
+const titleElement = document.querySelector('.profile__name');
 
- const aboutElement = document.querySelector('.profile__about');
- const nameAboutFieldElement = document.querySelector('[name="nameAbout"]')
+const aboutElement = document.querySelector('.profile__about');
 
- // console.log(editButton)
- // console.log(popup)
- // console.log(closePopButton)
+const formElement = document.querySelector('.popup__form');
+const nameFieldElement = formElement.querySelector('[name="name"]');
+const nameAboutFieldElement = formElement.querySelector('[name="nameAbout"]');
 
- editButton.addEventListener('click', function () {
-     popup.classList.add('popup_isOpen')
-     nameFieldElement.value = titleElement.textContent;
-     nameAboutFieldElement.value = aboutElement.textContent;
- } )
 
- closePopButton.addEventListener('click', function () {
-     popup.classList.remove('popup_isOpen')
- })
+editButton.addEventListener('click', function () {
+    openPopup(popup)
+    nameFieldElement.value = titleElement.textContent;
+    nameAboutFieldElement.value = aboutElement.textContent;
+})
+
+closePopButton.addEventListener('click', function () {
+    closePopup(popup)
+})
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+document.body.addEventListener('click', function (event) {
+    detectClickOutside(event)
+})
+
+
+function openPopup(popupElement) {
+    popupElement.classList.add('popup_opened');
+}
+
+function closePopup(popupElement) {
+    popupElement.classList.remove('popup_opened')
+}
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+    titleElement.textContent = nameFieldElement.value;
+    aboutElement.textContent = nameAboutFieldElement.value;
+    closePopup(popup)
+}
+
+function detectClickOutside(event) {
+    console.log(event.target)
+    if (event.target.classList.contains('popup')) {
+        closePopup(popup);
+    }
+}
