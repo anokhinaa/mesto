1ddb8a56821b4fa52eb713372ffdac1afb94da22
asyaanokhina <asya.anokhina@gmail.com>
2022-06-05 20:29:32 +0300

    refactor: Cards initialization from array :*

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,63 @@
+const initialCards = [
+    {
+        name: 'Австралия',
+        alt: 'Австралия',
+        link: './images/AUSTRALIA_3.JPG'
+    },
+    {
+        name: 'Алтай',
+        alt: 'Алтай',
+        link: './images/ALTAI_1.JPG'
+    },
+    {
+        name: 'Рио-де-Жанейро',
+        alt: 'Рио',
+        link: './images/RIO_1.JPG'
+    },
+    {
+        name: 'Париж',
+        alt: 'Париж',
+        link: './images/PARIS_1.JPG'
+    },
+    {
+        name: 'Сумароково, Кострома',
+        alt: 'Кострома',
+        link: './images/SUMAROKOVO_2.JPG'
+    },
+    {
+        name: 'Лима',
+        alt: 'Lima',
+        link: './images/LIMA_2.JPG'
+    }
+];
+
+
 const editButton = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
 const closePopButton = document.querySelector('.popup__close');
-
 const titleElement = document.querySelector('.profile__name');
-
 const aboutElement = document.querySelector('.profile__about');
-
 const formElement = document.querySelector('.popup__form');
 const nameFieldElement = formElement.querySelector('[name="name"]');
 const nameAboutFieldElement = formElement.querySelector('[name="nameAbout"]');
+const cardsTemplateElement = document.querySelector('.cards__template');
+const cardsSection = document.querySelector('.cards');
+
+function createCardElement (name, alt, link) {
+    const element = cardsTemplateElement.content.querySelector('.cards__element').cloneNode(true);
+    const img = element.querySelector ('.cards__image');
+    const title = element.querySelector ('.cards__title');
+    img.src = link;
+    img.alt = alt;
+    title.textContent = name;
+    return element;
+}
+
+function addCardElement (name, alt, link) {
+    const element = createCardElement(name, alt, link);
+    cardsSection.append(element);
+}
+
 
 
 editButton.addEventListener('click', function () {
@@ -48,3 +97,7 @@ function detectClickOutside(event) {
         closePopup(popup);
     }
 }
+
+initialCards.forEach(e => {
+    addCardElement(e.name, e.alt, e.link);
+});
